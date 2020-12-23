2459d1d924b9f8eb11affbb6c2ae5d4e36c957c0
Aleix Alcacer <aalcacer@uji.es>
2020-12-23 18:11:05 +0000

    Commit inicial

---
+++ R/resta.R
@@ -0,0 +1,17 @@
+#' Resta entre dos números
+#'
+#' Esta función devuelve la resta entre dos números.
+#'
+#' @param a Primer número
+#' @param b Segundo número
+#'
+#' @return La resta entre a y b
+#' @export
+#'
+#' @examples
+#' resta(5.2, 6)
+#'
+#' @seealso \link{suma}
+resta <- function(a, b) {
+  return(a - b)
+}
---
+++ R/suma.R
@@ -0,0 +1,17 @@
+#' Suma entre dos números
+#'
+#' Esta función devuelve la suma entre dos números.
+#'
+#' @param a Primer número
+#' @param b Segundo número
+#'
+#' @return La suma entre a y b
+#' @export
+#'
+#' @examples
+#' suma(5, 3)
+#'
+#' @seealso \link{resta}
+suma <- function(a, b) {
+  return(a + b)
+}
